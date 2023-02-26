533511f024b5916d05d7380cee0b75a42bba77bf
al3xHub <alejandrobueno92@hotmail.com>
2023-02-26 18:18:59 +0100

    Primer commit!!:)

---
+++ src/holaMundo/HolaMundo.java
@@ -0,0 +1,23 @@
+package holaMundo;
+/**
+ * La función HolaMundo permite crear un texto cuando le doi a run
+ * @author alejandrobueno
+ * @author fulanito
+ */
+public class HolaMundo 
+{
+
+	public static void main(String[] args)
+	{
+		// Imprimimos por consola un mensaje.
+		/**
+		 * qwertyuiyblablabla
+		 */
+
+		System.out.println("!hola mundo!");
+
+	}
+/*
+ * Comentario interno multilinea
+ */
+}
